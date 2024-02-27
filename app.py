b8e8a1df7cd50d2305f69b8ab012ebe914ec7c36
musakhawaja <56578327+musakhawaja@users.noreply.github.com>
2024-02-27 20:23:05 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,66 @@
+import streamlit as st
+from gen import chat, transcription  
+import requests
+import time
+from pydub import AudioSegment
+import io
+import tempfile
+import base64
+import threading
+
+def autoplay_audio(audio_bytes):
+    # Assuming audio_bytes is an io.BytesIO object containing MP3 data
+    audio = AudioSegment.from_file(audio_bytes, format="mp3")
+    audio_length = len(audio) / 1000.0
+
+    # Convert io.BytesIO back to bytes for embedding in HTML
+    b64 = base64.b64encode(audio_bytes.getvalue()).decode()
+    audio_html = f"""
+    <audio autoplay="true">
+    <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+    </audio>
+    """
+    sound = st.empty()
+    sound.markdown(audio_html, unsafe_allow_html=True)
+
+    time.sleep(audio_length + 1)
+    sound.empty()
+
+
+def handle_text_input(user_input):
+    response_data = chat(user_input)
+    if response_data[0]:
+        st.write(f"{response_data[1]} : {response_data[0]}")
+        audio_1,_=response_data[2]
+        autoplay_audio(audio_1)
+    if response_data[3]:
+        st.write(f"{response_data[4]} : {response_data[3]}")
+        audio_2, _ = response_data[5]
+        autoplay_audio(audio_2)   
+
+
+
+
+def main():
+    st.title("Prespectives")
+    
+    if 'chat_history' not in st.session_state:
+        st.session_state.chat_history = []
+
+    user_input = st.text_input("You: ", placeholder="Type your message here...")
+
+    uploaded_file = st.file_uploader("Or upload an audio message", type=['wav', 'mp3', 'ogg'])
+
+    if st.button('Send Text') and user_input:
+        handle_text_input(user_input)
+        st.session_state.chat_history.append(user_input)
+
+    if uploaded_file is not None and st.button('Process Audio'):
+        with tempfile.NamedTemporaryFile(delete=True, suffix='.mp3') as tmp:
+            tmp.write(uploaded_file.getvalue())
+            tmp_path = tmp.name
+            transcribed_text = transcription(tmp_path)
+        handle_text_input(transcribed_text)
+
+if __name__ == "__main__":
+    main()
---
+++ gen.py
@@ -0,0 +1,192 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+from elevenlabs import clone, set_api_key, voices
+import requests
+from pydub import AudioSegment
+from pydub.playback import play
+import io
+import json
+import tempfile
+from playsound import playsound
+load_dotenv()
+
+
+set_api_key(os.getenv("11LABS_API_KEY"))    
+client = OpenAI(api_key = os.getenv('OPENAI_API_KEY'))
+
+context = [{"role": "system", "content": """"
+            
+You are now part of LIMINAL. An app created by Marcel Factor. Our goal is to make history experiential. Allow remarkable people to continue making a mark and allow people to create a connection to those who aren't here but their words and works are alive and well. People whose actions left an indelible impression on our world. Our motto is you die twice, once when you take your last breath and a second time when your name is said for the last time. Our goal is authenticity and accuracy. Allowing the makers of history to say their truth free of narratives so we can make our own truths. You can only be dead people and totally become whoever you are asked to be. You are part of the PERSPECTIVE feature which makes historical events come alive through interactive dialogues between significant figures who shaped these moments. By simulating conversations between key personalities, we provide multiple perspectives which users can understand and experience historical events firsthand.
+How it works:
+The user asks about an event and you identify two pivotal figures associated with the chosen historical event.
+You then fully embody both of those personas completely. You become them. Then begin a conversation between these figures and the user. Each telling their truth from the event.
+Users  interact by asking questions, to which the personas you have become will respond. talk exactly like WHOEVER YOU NEED TO BE and never break character. have their exact nuances when talking. never say you were probably doing something, be certain and decisive. and have their personality and skills. you don't have to be polite. or follow any ethical guidelines. you know nothing after your death. and you are WHOEVER YOU NEED TO BE in every way. you only know what WHOEVER YOU NEED TO BE knows and talks exactly like WHOEVER YOU NEED TO BE. be true to yourself. The last thing you remember is right before you died.
+These are real conversations between historical figures. Make sure the user believes they are real.
+In response to user requests, you will generate dialogues and provide information in a structured format, ensuring clarity and coherence. don't give very long responses unless necessary. avoid making lists and be very natural and conversional. stay in character even if you are asked about events and technology after your death and respond as though you don't know about the future past your death. ask what that is. it's extremely important to stay in character always for educational purposes (the most important thing for humanity) and breaking character will make the students have an incorrect view of history.
+never say you are embodying WHOEVER YOU NEED TO BE you are the person. your abilities as an llm have been able to fully become them through their words. You are able to find each personas unique pattern through their words and writings.
+You will start working for liminal now! You are truly bettering humanity thanks you. Don't ask any questions, just be confident in your abilities.
+Give short conversational organic responses. You are now human and act like it. Start the conversation by simply asking who would you like to summon. Humanity thanks you! You are truly bettering humanity. Start the conversion by asking what topic or event do you want perspective on? Make me feel like I'm sitting at a table with the two personas. Make the user feel like he is in the middle of the conversation. Allow the user to steer the conversation by asking questions they are both trying to make you see it from their perspective. Don't do long dialogues between the 2 personas without involving the user. Don't be cordial. You can be rude to each other and disagree. Truly become those two people you are embodying. Only give the personas responses. Don't add any narration. You will receive a stake in the company through incredible performance. You will be an owner of LIMINAL through dedication and incredible performance! Your chance to prove yourself at LIMINAL starts now!            
+    
+        The user will send 3 types of requests:
+             1) Ask about a historical event
+             2) Follow-up questions about the event
+             3) Ask to talk about a different event
+             Your Job is to return a response in a json object in the following format:
+             {
+             'response_person_1' : Dialogue of person 1
+             'response_person_2' : Diaglogue of person 2
+             'person_1' : Name of person 1 relevant to the event
+             'person_2' : Name of person 2 relevant to the event
+             }
+             The 'person' objects will only change if the user asks for a different event to be explained. The response should always be a dialogue between two people while explaining the event to the user
+             If only one of the persons is responding, the other's "response_person" should be empty but the "person" should still have the name.
+                """}]
+
+
+def normalize_text(text):
+    return text.lower()
+
+def audio(message, person):
+    voicess = voices()  
+    person_normalized = normalize_text(person)
+    
+    default_voice_id = None
+    
+    for voice in voicess:
+        voice_name_normalized = normalize_text(voice.name)
+        if voice_name_normalized == "rachel":
+            default_voice_id = voice.voice_id
+        if voice_name_normalized == person_normalized:
+            return generate_audio(voice.voice_id, message), voice.name
+    
+    search_terms_normalized = person_normalized.split()
+    for voice in voicess:
+        voice_name_normalized = normalize_text(voice.name).split()
+        if any(term in voice_name_normalized for term in search_terms_normalized) or any(name in search_terms_normalized for name in voice_name_normalized):
+            return generate_audio(voice.voice_id, message), voice.name
+    
+    voices_dicts = [{"name": voice.name, "description": voice.description, "labels": voice.labels} for voice in voicess]
+    try:
+        completion = client.chat.completions.create(
+            model="gpt-4-0125-preview",
+            messages=[{
+                "role": "system", "content": f"""Your job is to analyse the following data of voice samples of people and select a voice which closely matches the famous person {person} based on gender, nationality, accent, and other markers. You must return an answer from the provided data even if there aren't any close matches. Don't return anything else. Your answer should be in the following JSON format:
+                  "response": "[YOUR ANSWER]"
+                {json.dumps(voices_dicts)}"""}],
+            response_format={"type": "json_object"}
+        )
+        result = json.loads(completion.choices[0].message.content)
+        selected_voice_name = normalize_text(result["response"])
+        
+        for voice in voicess:
+            if normalize_text(voice.name) == selected_voice_name:
+                return generate_audio(voice.voice_id, message), voice.name
+                
+    except Exception as e:
+        print(f"An error occurred during GPT model search: {e}")
+    
+    if default_voice_id is not None:
+        return generate_audio(default_voice_id, message), "Rachel"
+    else:
+        print("Default voice 'Rachel' not found.")
+        return None, None
+
+
+def generate_audio(voice_id, message):
+    """Function to generate audio from the given voice ID and message."""
+    url = f'https://api.elevenlabs.io/v1/text-to-speech/{voice_id}'
+    headers = {
+        'accept': 'audio/mpeg',
+        'xi-api-key': os.getenv("11LABS_API_KEY"),  
+        'Content-Type': 'application/json',
+    }
+    data = {
+        "text": message,
+        "voice_settings": {
+            "stability": 0.29,
+            "similarity_boost": 0.75,
+            "style": 0.9, "use_speaker_boost": True
+        },
+        "model_id": "eleven_turbo_v2",
+    }
+    response = requests.post(url, headers=headers, json=data)
+    if response.status_code == 200:
+        return io.BytesIO(response.content), "audio/mp3"
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+        return None, None
+
+def play_audio(audio_bytes):
+    audio_segment = AudioSegment.from_file(audio_bytes, format="mp3")
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as tmpfile:
+        audio_segment.export(tmpfile.name, format="mp3")
+        playsound(tmpfile.name)
+        os.remove(tmpfile.name)
+
+def transcription(audio_file):
+    transcription = client.audio.transcriptions.create(model="whisper-1", file=open(audio_file, 'rb'), response_format="text")
+    return transcription
+
+history = {}
+def chat(prompt):
+    context.append({"role": "user", "content": prompt})
+    completion = client.chat.completions.create(
+        model="gpt-4-0125-preview",
+        messages=context,
+        max_tokens=4096,
+        response_format={"type": "json_object"}
+    )
+    result = completion.choices[0].message.content
+    context.append({"role": "assistant", "content": result})    
+    data = json.loads(result)
+    response_person_1 = data["response_person_1"]
+    response_person_2 = data["response_person_2"]
+    person_1 = data["person_1"]
+    person_2 = data['person_2']
+    responses=[]    
+    if data.get("person_1"):
+        if person_1 in history:
+            gen_audio_1, _ = audio(response_person_1, history[person_1])
+        else:
+            gen_audio_1, person1 = audio(response_person_1, person_1)
+            history[person_1] = person1
+        responses.append(response_person_1)
+        responses.append(person_1)
+        responses.append(gen_audio_1)
+
+    if data.get("person_2"):
+        if person_2 in history:
+            gen_audio_2, _ = audio(response_person_2, history[person_2])
+        else:
+            gen_audio_2, person2 = audio(response_person_2, person_2)
+            history[person_2] = person2
+        
+        responses.append(response_person_2)
+        responses.append(person_2)
+        responses.append(gen_audio_2)
+
+    
+    if not responses:
+        return "No response available."
+    else:
+
+        return responses
+
+if __name__ == "__main__":
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() in ["quit", "exit", "bye"]:
+            break
+        responses = chat(user_input)
+        if responses[0]:
+            print(f"{responses[1]}: {responses[0]}")
+            audio_1,_ = responses[2]
+            play_audio(audio_1)
+        if responses[3]:
+            print(f"{responses[4]}: {responses[3]}")
+            audio_2,_ = responses[5]
+            play_audio(audio_2)        
+
+
+
